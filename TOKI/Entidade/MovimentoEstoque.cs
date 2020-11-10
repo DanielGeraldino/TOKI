943fc5dd75a7857b9db88c6c028f5b38d8e14e63
ValerioZucoloto <zucolotovalerio@gmail.com>
2020-11-10 19:52:23 -0300

    Criação da classe MovimentoEstoque

---
+++ TOKI/Entidade/MovimentoEstoque.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TOKI.Entidade {
+    class MovimentoEstoque {
+        public int movimento { get; set; }
+        public Produto produto { get; set; }
+        public DateTime data { get; set; }
+        public Almoxarifado almoxarifado { get; set; }
+        public float quantidade { get; set; }
+        public Usuario usuario { get; set; }
+
+
+        public MovimentoEstoque() {
+        }
+
+        public MovimentoEstoque(int movimento, Produto produto, DateTime data, Almoxarifado almoxarifado, float quantidade, Usuario usuario) {
+            this.movimento = movimento;
+            this.produto = produto;
+            this.data = data;
+            this.almoxarifado = almoxarifado;
+            this.quantidade = quantidade;
+            this.usuario = usuario;
+        }
+
+        public string descreveMovimento() {
+            return "Entrada ou saída ? "
+                + movimento +
+                "Produto: " +
+                produto +
+                "Data de processamento: " +
+                data +
+                almoxarifado +
+                "quantidade: " +
+                quantidade +
+                "Usuário: " +
+                usuario;
+        }
+
+
+
+
+    }
+}
