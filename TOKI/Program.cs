4c5dded3e3603143aff5a6dd8b5ca8885da36d4b
GabrielAlves98 <73914262+GabrielAlves98@users.noreply.github.com>
2020-11-23 22:11:24 -0300

    Conexao Mysql/Autenticação

--- TOKI/Program.cs
+++ TOKI/Program.cs
@@ -1,18 +1,54 @@
 ﻿using System;
 using TOKI.Entidade;
 using TOKI.Visualizacao;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
 
 namespace TOKI
 {
     class Program
     {
+
         static void Main(string[] args)
         {
 
-            Almoxarifado a = new Almoxarifado("teste", "teste end");
-            Tela t = new Tela(a);
+            // connection con = new connection();
 
-            t.TelaMenu();
+            
+            string sql = " SELECT * FROM usuarios  ";
+            MySqlConnection con = new MySqlConnection("host=localhost;user=root;password=admin;database=toki;");
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            inicio:
+            Console.WriteLine("Digite o seu login:");
+            string login = Console.ReadLine();
+            Console.WriteLine("Digite a sua senha:");
+            string senha = Console.ReadLine();
+            //MySqlDataReader reader = cmd.ExecuteReader();
+            MySqlDataAdapter sda = new MySqlDataAdapter("select count(*) from usuarios where login ='"+login+"'AND senha='"+senha+"'",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            
+            
+            if (dt.Rows[0][0].ToString()=="1")
+            {
+                Console.WriteLine("Usuario e Senha validos");
+                Console.Clear();
+                Almoxarifado a = new Almoxarifado("teste", "teste end");
+                Tela t = new Tela(a);
+
+                t.TelaMenu();
+            }
+            else
+            {
+                
+                Console.Clear();
+                Console.WriteLine("Usuario ou Senha invalido");
+                goto inicio;
+            }
+
         }
     }
 }
